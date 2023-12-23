72026c4ab7a6aaf18d2df106feb1537fb6c5cd7e
SAJID HUSSAIN <53827080+sajidhussaint@users.noreply.github.com>
2023-12-23 18:00:35 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Hero() {
     return (
         <div className="hero">
-             <img src='http://tiny.cc/burger-bg' alt="bir" />
+             <img src='https://images.unsplash.com/photo-1544025162-d76694265947?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt="bir" />
              <div className="hero-content">
                  <h1>Its all about good food & taste</h1>
                  <p>As soon as you mention the extra efforts required to prepare a particular dish, it automatically becomes special. Especially in fine-dine restaurants, customers come for the entire guest experience.</p>
